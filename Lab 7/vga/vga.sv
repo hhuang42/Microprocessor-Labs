1f023fa623d70245b2548331196817b99935aef5
hhuang42 <hhuang@g.hmc.edu>
2014-10-31 23:46:04 -0700

    Finished Lab 7
    It has begun

--- Lab 7/vga/vga.sv
+++ Lab 7/vga/vga.sv
@@ -3,12 +3,21 @@
 // VGA driver with character generator
 
 module vga(input  logic       clk,
-			  output logic       vgaclk,						// 25 MHz VGA clock
-			  output logic       hsync, vsync, sync_b,	// to monitor & DAC
-			  output logic [7:0] r, g, b);					// to video DAC
+           input  logic       spi_clk,
+           input  logic       spi_in,
+           input  logic       spi_fsync,
+           output logic [7:0] led,
+           output logic       vgaclk,						// 25 MHz VGA clock
+           output logic       hsync, vsync, sync_b,	// to monitor & DAC
+           output logic [7:0] r, g, b);					// to video DAC
  
-  logic [9:0] x, y;
-  logic [7:0] r_int, g_int, b_int;
+  logic [9:0]  x, y;
+  logic [7:0]  r_int, g_int, b_int;
+  logic [31:0] spi_word;
+  logic [9:0]  x_cursor;
+  logic [9:0]  y_cursor;
+  logic        finished;
+  logic [3:0]  buttons;
 	
   // Use a PLL to create the 25.175 MHz VGA pixel clock 
   // 25.175 Mhz clk period = 39.772 ns
@@ -21,8 +30,18 @@ module vga(input  logic       clk,
   vgaController vgaCont(vgaclk, hsync, vsync, sync_b,  
                         r_int, g_int, b_int, r, g, b, x, y);
 	
+
+  
+  spi_frm_slave slave(spi_clk, spi_in, spi_fsync, spi_word, finished);
+  
+  mouse_reader reader(vsync, spi_word, finished, x_cursor, y_cursor, buttons);
+  
   // user-defined module to determine pixel color
-  videoGen videoGen(x, y, r_int, g_int, b_int);
+  videoGen videoGen(vsync, x, y, x_cursor, y_cursor, r_int, g_int, b_int);
+  
+  assign led[2:0] = buttons;
+  assign led[3] = finished;
+  assign led[4] = spi_fsync;
 endmodule
 
 module vgaController #(parameter HMAX   = 10'd800,
@@ -32,10 +51,10 @@ module vgaController #(parameter HMAX   = 10'd800,
 											VSTART = 10'd37,
 											HEIGHT = 10'd480)
 						  (input  logic       vgaclk, 
-                     output logic       hsync, vsync, sync_b,
-							input  logic [7:0] r_int, g_int, b_int,
-							output logic [7:0] r, g, b,
-							output logic [9:0] x, y);
+               output logic       hsync, vsync, sync_b,
+							 input  logic [7:0] r_int, g_int, b_int,
+							 output logic [7:0] r, g, b,
+							 output logic [9:0] x, y);
 
   logic [9:0] hcnt, vcnt;
   logic       oldhsync;
@@ -67,36 +86,119 @@ module vgaController #(parameter HMAX   = 10'd800,
   assign {r,g,b} = valid ? {r_int,g_int,b_int} : 24'b0;
 endmodule
 
-module videoGen(input  logic [9:0] x, y,
-           		 output logic [7:0] r_int, g_int, b_int);
-	
-  logic pixel;
+module videoGen(input  logic       clk,
+                input  logic [9:0] x, y, x_cursor, y_cursor,
+           		  output logic [7:0] r_int, g_int, b_int);
+
   logic [7:0] ch;
-
-  // given y position, choose a character to display
-  // then look up the pixel value from the character ROM
-  // and display it in red or blue
-  assign ch = y[8:3]+8'd48;  
-  chargenrom chargenromb(ch, x[2:0], y[2:0], pixel);  
-  assign {r_int, g_int, b_int} = (y[3]==0) ? {{8{pixel}},16'h0000} : 
-	                                           {16'h0000,{8{pixel}}}; 
-endmodule
-
-module chargenrom(input  logic [7:0] ch,
-                  input  logic [2:0] xoff, yoff,
-						output logic       pixel);
-						
-  logic [5:0] charrom[2047:0]; // character generator ROM
-  logic [7:0] line;            // a line read from the ROM
+  logic [23:0] background_rgb;
   
-  // initialize ROM with characters from text file 
-  initial 
-	 $readmemb("charrom.txt", charrom);
-	 
-  // index into ROM to find line of character
-  assign line = {charrom[yoff+{ch, 3'b000}]}; 
-  // reverse order of bits
-  assign pixel = line[3'd7-xoff];
+  background(clk, x, y, background_rgb);
+  draw_cursor(x, y, x_cursor, y_cursor, background_rgb, {r_int, g_int, b_int});
 endmodule
+
+module draw_cursor(input  logic [9:0]  x, y, x_cursor, y_cursor,
+                   input  logic [23:0] background_rgb,
+                   output logic [23:0] rgb);
+  logic in_cursor;
+  logic in_hitbox;
+  in_ellipse cursor(x, y, x_cursor, y_cursor, 140,1,0, in_cursor);
+  in_disk hitbox(x, y, x_cursor, y_cursor, 8, in_hitbox);
+  assign rgb = in_hitbox ? 24'hFFFFFF : 
+               in_cursor ? {8'((9'hFF+background_rgb[23:16])>>1),
+                            8'((9'h80+background_rgb[15: 8])>>1),
+                            8'((9'h80+background_rgb[ 7: 0])>>1)}: 
+               background_rgb;
+                   
+endmodule
+
+module in_disk  (input  logic [9:0]  x, y, cent_x, cent_y,
+                 input  logic [21:0] rad_squared,
+                 output logic        is_in);
+  logic signed [10:0] d_x, d_y;
+  logic [21:0] dist_squared;
+  always_comb begin
+    d_x = (x-cent_x);
+    d_y = (y-cent_y);
+    dist_squared = d_x*d_x + d_y*d_y;
+    is_in = (dist_squared < rad_squared);
+  end
+endmodule
+
+module in_ellipse  (input  logic [9:0]  x, y, cent_x, cent_y,
+                    input  logic [31:0] rad_squared,
+                    input  logic [1:0]  x_reduce, y_reduce,
+                    output logic        is_in);
+  logic signed [15:0] d_x, d_y;
+  logic [31:0] dist_squared;
+  always_comb begin
+    d_x = (x-cent_x) << x_reduce;
+    d_y = (y-cent_y) << y_reduce;
+    dist_squared = d_x*d_x + d_y*d_y;
+    is_in = (dist_squared < rad_squared);
+  end
+endmodule
+
+module background(input  logic       clk,
+                  input  logic [9:0] x_screen, y_screen,
+                  output logic [23:0] rgb);
+logic [9:0] diff, sum, x, y;
+logic [19:0] product; 
+logic [7:0] r, g, b, x_wave, y_wave, intensity;
+logic [10:0] counter;
+
+always_ff @(posedge clk) begin
+  counter <= counter + 1;
+end
+
+always_comb begin
+  x = x_screen;
+  y = y_screen;
+  diff = (x - y + (counter >> 0));
+  sum  = (x+y - (counter >> 0));
+  product = diff*sum;
+  intensity = 8'(product[11:4] + (counter << 2)) >> 3;
+  x_wave = (8'((x<<2)-counter)>>>5) + (8'(-(x<<2)-counter)>>>5);
+  y_wave = (8'((y<<2)-counter)>>>6) + (8'(-(y<<2)-counter)>>>6);
+  
+  r = 8'h70 + x_wave + y_wave;
+  g = 8'h80 + ~intensity;
+  b = 8'hC0 + intensity + x_wave + y_wave;
+  rgb = {r,g,b};
+end
+
+endmodule
+
+module spi_frm_slave #(parameter WIDTH_POWER = 5, WIDTH = 2**WIDTH_POWER)
+                      (input  logic               spi_clk,
+                       input  logic               serial_input,
+                       input  logic               fsync,
+                       output logic [(WIDTH-1):0] word_input,
+                       output logic               finished);
+  logic [(WIDTH_POWER-1):0] counter = 0;
+  
+  always_ff @ (posedge spi_clk) begin
+    word_input <= finished ? word_input : 
+                            {word_input[(WIDTH-2):0], serial_input};
+    counter <= finished ? 1 : counter + 1;
+    finished <= ~fsync ? 0 : finished ? 1 : counter == 0;
+  end
+  
+endmodule
+
+module mouse_reader  (input  logic        sync_clk,
+                      input  logic [31:0] spi_word,
+                      input  logic        finished,
+                      output logic [9:0]  x_pixel, y_pixel,
+                      output logic [2:0]  button_state);
+  logic [22:0] complete_word;
+  always_ff @ (posedge finished) begin
+    complete_word <= spi_word[22:0];
+  end
+  always_ff @ (posedge sync_clk) begin
+    {button_state, x_pixel, y_pixel} = complete_word;
+  end
+endmodule
+  
 
 
